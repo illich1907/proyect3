0ce50b222c5a45ac19207fd4613eab065c442a41
illich1907 <anastasiazatovkanuk@gmail.com>
2024-07-03 17:54:12 +0300

    Create Edit Void

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 void banana(char* str, const char* charsToRemove) {
+    int a = 0;
 
+    int b = 0;
+    while (str[b] != '\0') {
+        b++;
+    }
+
+    int c = 0;
+    while (charsToRemove[c] != '\0') {
+        c++;
+    }
+
+    for (int i = 0; i < b; ++i) {
+        bool d = false;
+        for (int j = 0; j < c; ++j) {
+            if (str[i] == charsToRemove[j]) {
+                d = true;
+                break;
+            }
+        }
+        if (!d) {
+            str[a++] = str[i];
+        }
+    }
+    str[a] = '\0';
 }
 int main() {
 
